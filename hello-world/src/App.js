25ffecc18b4eec68e29412001af2b13ea9b75f2d
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-05-14 23:04:37 +0530

    React

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -7,6 +7,7 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 class App extends Component {
   render() {
@@ -37,8 +38,11 @@ class App extends Component {
 
         {/* <Counter /> */}
 
-        <FunctionClick />
-        <ClassClick />
+        {/* <FunctionClick /> */}
+        {/* <ClassClick /> */}
+
+        <EventBind />
+
        
        
       </div>
