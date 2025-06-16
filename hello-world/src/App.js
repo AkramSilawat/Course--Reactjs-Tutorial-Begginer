24bfc0e4e54081d2b0cec43b7c1d23a91d5fc6bc
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-06-16 21:37:26 +0530

    React

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -21,6 +21,7 @@ import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 
 class App extends Component {
@@ -78,7 +79,9 @@ class App extends Component {
 
       {/* <PureComp /> */}
 
-      <ParentComp />
+      {/* <ParentComp /> */}
+
+      <RefsDemo />
 
 
       </div>
---
+++ hello-world/src/components/input.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class Input extends Component {
+    constructor(props){
+        super(props)
+        this.inputRef = React.createRef()
+    }
+    render() {
+        return (
+            <div>
+                <input type="text" ref={this.inputRef}></input>
+            </div>
+        )
+    }
+}
+
+export default Input
