8dc7ddfc07b6efc2a3600d5a8e0a34cd4b36084c
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-06-21 09:43:03 +0530

    Ract

--- hello-world/src/components/input.js
+++ hello-world/src/components/input.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 
-export class Input extends Component {
+class Input extends Component {
     constructor(props){
         super(props)
         this.inputRef = React.createRef()
+    }
+
+    focusInput() {
+        this.inputRef.current.focus()
     }
     render() {
         return (
