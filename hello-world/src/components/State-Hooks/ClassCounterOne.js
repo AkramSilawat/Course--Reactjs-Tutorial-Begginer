9bfcefdac038b062295a451ace8da284ecd836c4
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-08-11 19:35:48 +0530

    Conditionally Run Effects

--- hello-world/src/components/State-Hooks/ClassCounterOne.js
+++ hello-world/src/components/State-Hooks/ClassCounterOne.js
@@ -2,11 +2,42 @@ import React, { Component } from "react";
 
 
 
+// class ClassCounterOne extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             count: 0
+//         }
+//     }
+
+//     componentDidMount() {
+//         document.title = `Clicked ${this.state.count} times`
+//     }
+
+//     componentDidUpdate(prevProps, prevState){
+//         document.title = `Clicked ${this.state.count} times`
+//     }
+
+//     render(){
+//         return (
+//             <div>
+//                 <button onClick={() => this.setState({ count: this.state.count + 1})}>
+//                     Click {this.state.count} times
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
+
+
+// ---> 8. Conditionally Run Effects
+
 class ClassCounterOne extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            count: 0
+            count: 0,
+            name: ''
         }
     }
 
@@ -14,14 +45,24 @@ class ClassCounterOne extends Component {
         document.title = `Clicked ${this.state.count} times`
     }
 
-    componentDidUpdate(prevProps, prevState){
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count === this.state.count) {
+            console.log('Updating document title')
+        }
         document.title = `Clicked ${this.state.count} times`
     }
-  
-    render(){
+
+    render() {
         return (
             <div>
-                <button onClick={() => this.setState({ count: this.state.count + 1})}>
+                <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={e => {
+                        this.setState({ name: e.target.value })
+                    }}
+                />
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
                     Click {this.state.count} times
                 </button>
             </div>
