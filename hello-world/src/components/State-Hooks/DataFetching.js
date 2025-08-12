8c89595d42a35c2444f7a575e0576420c1c16f8b
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-08-12 12:24:57 +0530

    Fetching data with useEffect Part 2

--- hello-world/src/components/State-Hooks/DataFetching.js
+++ hello-world/src/components/State-Hooks/DataFetching.js
@@ -1,26 +1,53 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function DataFetching(){
-    const [posts, setPosts] = useState([])
+// function DataFetching() {
+//     const [posts, setPosts] = useState([])
+
+//     useEffect(() => {
+//         axios
+//             .get('https://jsonplaceholder.typicode.com/posts')
+//             .then(res => {
+//                 console.log(res)
+//                 setPosts(res.data)
+//             })
+//             .catch(err => {
+//                 console.log(err)
+//             })
+//     }, [])
+
+//     return (
+//         <div>
+//             <ul>
+//                 {posts.map(post => (
+//                     <li key={post.id}>{post.title}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+//---->  Part - 2 <---- 
+
+function DataFetching() {
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-            console.log(res)
-            setPosts(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }, [])
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => {
+                console.log(res)
+                setPost(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [id])
     return (
         <div>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
+            <input type="text" value={id} onChange={e => setId(e.target.value)} />
+            <div>{post.title}</div>
         </div>
     )
 }
