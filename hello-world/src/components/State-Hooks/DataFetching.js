934dd46f2ec32c72cd922f65fd12effb35422e38
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-08-12 12:36:26 +0530

    Fetching data with useEffect Part 3

--- hello-world/src/components/State-Hooks/DataFetching.js
+++ hello-world/src/components/State-Hooks/DataFetching.js
@@ -28,11 +28,41 @@ import axios from "axios";
 // }
 
 
-//---->  Part - 2 <---- 
+// //---->  Part - 2 <---- 
+
+// function DataFetching() {
+//     const [post, setPost] = useState({})
+//     const [id, setId] = useState(1)
+
+//     useEffect(() => {
+//         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//             .then(res => {
+//                 console.log(res)
+//                 setPost(res.data)
+//             })
+//             .catch(err => {
+//                 console.log(err)
+//             })
+//     }, [id])
+//     return (
+//         <div>
+//             <input type="text" value={id} onChange={e => setId(e.target.value)} />
+//             <div>{post.title}</div>
+//         </div>
+//     )
+// }
+
+
+//---->  Part - 3 <---- 
 
 function DataFetching() {
     const [post, setPost] = useState({})
     const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -43,10 +73,12 @@ function DataFetching() {
             .catch(err => {
                 console.log(err)
             })
-    }, [id])
+    }, [idFromButtonClick])
+
     return (
         <div>
             <input type="text" value={id} onChange={e => setId(e.target.value)} />
+            <button type="button" onClick={handleClick}>Fetch Post</button>
             <div>{post.title}</div>
         </div>
     )
