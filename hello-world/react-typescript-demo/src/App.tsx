2dd712981aa9267df26e30c785b7aa56badc9d2a
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-09-12 20:52:13 +0530

    Basic Props

--- hello-world/react-typescript-demo/src/App.tsx
+++ hello-world/react-typescript-demo/src/App.tsx
@@ -5,8 +5,7 @@ import { Greet } from './components/Greet'
 function App() {
     return (
         <div clssName='App'>
-            <Greet name='Akram' messageCount={20} />
-
+            <Greet name='Akram' messageCount={20} isLoggedIn={false}/>
         </div>
     )
 }
