f55e750dce3cd1163ada0c288667a988067d257e
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-09-14 19:06:52 +0530

    Advanced Props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,34 +1,50 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
 
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Kent',
-    },
-    {
-      first: 'Princess',
-      last: 'Diana',
-    }
-  ]
+  // -------> Basic Props <------
+
+  // const personName = {
+  //   first: 'Bruce',
+  //   last: 'Wayne'
+  // }
+
+  // const nameList = [
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne',
+  //   },
+  //   {
+  //     first: 'Clark',
+  //     last: 'Kent',
+  //   },
+  //   {
+  //     first: 'Princess',
+  //     last: 'Diana',
+  //   }
+  // ]
 
   return (
     <div className="App">
-      <Greet name='Akram' messageCount={20} isLoggedIn={false} />
+
+   {/* -------> Basic Props <------ */}
+
+      {/* <Greet name='Akram' messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
-      <PersonList name={nameList}/>
+      <PersonList name={nameList}/> */}
+
+      <Status status='loading'/>
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes Leonardo Dispario!</Heading>
+      </Oscar>
+      <Greet name='Akram' isLoggedIn={false} />
     </div>
   );
 }
---
+++ react-typescript/src/components/Status.tsx
@@ -0,0 +1,19 @@
+type StateProps = {
+    status: 'loading' | 'success' | 'error'
+}
+
+export const Status = (props: StateProps) => {
+    let message 
+    if(props.status === 'loading') {
+        message = 'Loading...'
+    }else if (props.status === 'success') {
+        message = 'Data fetched successfully!'
+    }else if (props.status === 'error') {
+        message = 'Error fetching data'
+    }
+    return (
+        <div>
+            <h2>Status - {message}</h2>
+        </div>
+    )
+}
