54baee8ff5defbe894aa83883a5787be074a805a
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-09-15 12:28:19 +0530

    Event Props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
+import { Button } from './components/Button';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
+import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
@@ -33,20 +35,26 @@ function App() {
   return (
     <div className="App">
 
-   {/* -------> Basic Props <------ */}
+      {/* -------> Basic Props <------ */}
 
       {/* <Greet name='Akram' messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList name={nameList}/> */}
 
-      <Status status='loading'/>
+      {/* <Status status='loading'/>
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading>Oscar goes Leonardo Dispario!</Heading>
       </Oscar>
-      <Greet name='Akram' isLoggedIn={false} />
+      <Greet name='Akram' isLoggedIn={false} /> */}
+
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id)
+      }}
+      />
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
-  );
+  )
 }
 
 export default App;
