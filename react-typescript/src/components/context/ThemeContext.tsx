46857e7b75b5a21bf8b86dac0e53e0e011480653
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-09-18 22:16:22 +0530

    useContext Hook

---
+++ react-typescript/src/components/context/ThemeContext.tsx
@@ -0,0 +1,8 @@
+import React, {createContext} from "react"
+import { theme } from "./theme"
+
+type ThemeContextProviderProps = {
+    children: React.ReactNode
+}
+
+const ThemeContext = createContext(theme)
