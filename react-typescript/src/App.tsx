fe093c80ca4d935b8109dacdb967a0b4a835aa3d
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-09-23 17:37:23 +0530

    Generic Props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -100,12 +100,32 @@ function App() {
 
       {/* <Private isLoggedIn={true} component={Profile} /> */}
 
- 
+
       {/* <List
         items={['Batman', 'Superman', 'Wonder Woman ']}
         onClick={(item) => console.log(item)}
       />
-       <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: 'Bruce',
+            last: 'Wayne',
+          },
+          {
+            id: 2,
+            first: 'Clark',
+            last: 'Kent',
+          },
+          {
+            id: 3,
+            first: 'Princess',
+            last: 'Diana',
+          },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   )
 }
