b3e4c220f73c98b0a86fd8bc010183eaa650ba56
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-09-23 17:36:14 +0530

    Generic Props

--- react-typescript/src/components/generics/List.tsx
+++ react-typescript/src/components/generics/List.tsx
@@ -1,21 +1,18 @@
-import { useState } from "react"
-
-type ListProps = {
-    items: string[]| number[]
-    onClick: (value: string | number) => void
+type ListProps<T> = {
+    items:  T[]
+    onClick: (value: T) => void
 }
-export const List = ({ items, onClick }: ListProps) => {
-    const [user, setUser] = useState({name: 'akram', city: 'pali'})
-    return (
+export const List = <T extends {id: number} >({ items, onClick }: ListProps<T>) => {
+   return (
         <div>
             <h2>List of items</h2>
             {items.map((item, index) => {
                 return (
-                    <div key={index} onClick={() => onClick(item)}>
-                        {item}
+                    <div key={item.id} onClick={() => onClick(item)}>
+                        {/* {item} */}
                     </div>
                 )
             })}
         </div>
     )
-}
+} 
